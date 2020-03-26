391176a3a78892c9c8c48aac15ffa6affcdcf879
Jyotirmoy Saha <jyotirmoy6jmy@gmail.com>
2020-03-26 20:46:42 +0530

    WIP: Multi tabs on chat

--- src/browser/widget.jsx
+++ src/browser/widget.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ipcRenderer } from 'electron';
 
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
 class WidgetContainer extends React.Component {
     constructor(props) {
         super(props);
@@ -87,22 +90,75 @@ class WidgetContainer extends React.Component {
 
         return (
 
-            <div className="wrapper">
-                <div className="header">
-                    <h2>{this.sender}</h2>
-                </div>
-                <div className="message-box">
-                    <p>{message.body}</p>
-                </div>
-                <div className="reply-box">
-                    <textarea
-                        className="replyInput"
-                        value={reply}
-                        onChange={this.handleMessageChange}
-                        onKeyDown={this.handleKeyDown}
-                    />
-                </div>
-            </div>
+          <div className="wrapper">
+
+            {/*<div className="header">*/}
+            {/*    <h2>{this.sender}</h2>*/}
+            {/*</div>*/}
+
+              {/*<div className="container">*/}
+              {/*    <ul className="nav nav-tabs">*/}
+              {/*        <li className="active"><a data-toggle="tab" href="#home">Home</a></li>*/}
+              {/*        <li><a data-toggle="tab" href="#menu1">Menu 1</a></li>*/}
+              {/*        <li><a data-toggle="tab" href="#menu2">Menu 2</a></li>*/}
+              {/*        <li><a data-toggle="tab" href="#menu3">Menu 3</a></li>*/}
+              {/*    </ul>*/}
+
+              {/*    <div className="tab-content">*/}
+              {/*        <div id="home" className="tab-pane fade in active">*/}
+              {/*            <h3>HOME</h3>*/}
+              {/*            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt*/}
+              {/*                ut labore et dolore magna aliqua.</p>*/}
+              {/*        </div>*/}
+              {/*        <div id="menu1" className="tab-pane fade">*/}
+              {/*            <h3>Menu 1</h3>*/}
+              {/*            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea*/}
+              {/*                commodo consequat.</p>*/}
+              {/*        </div>*/}
+              {/*        <div id="menu2" className="tab-pane fade">*/}
+              {/*            <h3>Menu 2</h3>*/}
+              {/*            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque*/}
+              {/*                laudantium, totam rem aperiam.</p>*/}
+              {/*        </div>*/}
+              {/*        <div id="menu3" className="tab-pane fade">*/}
+              {/*            <h3>Menu 3</h3>*/}
+              {/*            <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt*/}
+              {/*                explicabo.</p>*/}
+              {/*        </div>*/}
+              {/*    </div>*/}
+              {/*</div>*/}
+
+              <Tabs>
+                  <TabList>
+                      <Tab>{this.sender}</Tab>
+                      {/*<Tab>Title 2</Tab>*/}
+                  </TabList>
+
+                  <TabPanel>
+                      <div className="message-box">
+                          <p>{message.body}</p>
+                      </div>
+                      <div className="reply-box">
+                        <textarea className="replyInput" value={reply} onChange={this.handleMessageChange} onKeyDown={this.handleKeyDown}/>
+                      </div>
+                  </TabPanel>
+                  {/*<TabPanel>*/}
+                  {/*    <h2>Any content 2</h2>*/}
+                  {/*</TabPanel>*/}
+              </Tabs>
+
+            {/*  <div className="message-box">*/}
+            {/*    <p>{message.body}</p>*/}
+            {/*  </div>*/}
+            {/*<div className="reply-box">*/}
+            {/*    <textarea*/}
+            {/*        className="replyInput"*/}
+            {/*        value={reply}*/}
+            {/*        onChange={this.handleMessageChange}*/}
+            {/*        onKeyDown={this.handleKeyDown}*/}
+            {/*    />*/}
+            {/*</div>*/}
+          </div>
         );
     }
 }
