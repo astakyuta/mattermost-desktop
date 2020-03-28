01ddf397de66fecbf78b2b91a2dc57a2d895b8c6
Jyotirmoy Saha <jyotirmoy6jmy@gmail.com>
2020-03-28 21:26:26 +0530

    TASK: Appending messages in widget coming from same channel

--- src/browser/widget.jsx
+++ src/browser/widget.jsx
@@ -9,7 +9,8 @@ class WidgetContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            message: null,
+            // message: null,
+            message: [],
             reply: '',
         };
 
@@ -33,8 +34,11 @@ class WidgetContainer extends React.Component {
             // this.messageDetails.user_id = payload.message.channel.teammate_id;
 
             this.setState({
-                message: payload.message,
+                // message: payload.message,
+                message: this.state.message.concat(payload.message),
             });
+
+            // this.setState.message.push(payload.message);
         })
     }
 
@@ -84,6 +88,7 @@ class WidgetContainer extends React.Component {
 
     render() {
         const { message, reply } = this.state;
+        console.log('message is: ', message);
         if (!message) {
             return null;
         }
@@ -92,52 +97,23 @@ class WidgetContainer extends React.Component {
 
           <div className="wrapper">
 
-            {/*<div className="header">*/}
-            {/*    <h2>{this.sender}</h2>*/}
-            {/*</div>*/}
-
-              {/*<div className="container">*/}
-              {/*    <ul className="nav nav-tabs">*/}
-              {/*        <li className="active"><a data-toggle="tab" href="#home">Home</a></li>*/}
-              {/*        <li><a data-toggle="tab" href="#menu1">Menu 1</a></li>*/}
-              {/*        <li><a data-toggle="tab" href="#menu2">Menu 2</a></li>*/}
-              {/*        <li><a data-toggle="tab" href="#menu3">Menu 3</a></li>*/}
-              {/*    </ul>*/}
-
-              {/*    <div className="tab-content">*/}
-              {/*        <div id="home" className="tab-pane fade in active">*/}
-              {/*            <h3>HOME</h3>*/}
-              {/*            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt*/}
-              {/*                ut labore et dolore magna aliqua.</p>*/}
-              {/*        </div>*/}
-              {/*        <div id="menu1" className="tab-pane fade">*/}
-              {/*            <h3>Menu 1</h3>*/}
-              {/*            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea*/}
-              {/*                commodo consequat.</p>*/}
-              {/*        </div>*/}
-              {/*        <div id="menu2" className="tab-pane fade">*/}
-              {/*            <h3>Menu 2</h3>*/}
-              {/*            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque*/}
-              {/*                laudantium, totam rem aperiam.</p>*/}
-              {/*        </div>*/}
-              {/*        <div id="menu3" className="tab-pane fade">*/}
-              {/*            <h3>Menu 3</h3>*/}
-              {/*            <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt*/}
-              {/*                explicabo.</p>*/}
-              {/*        </div>*/}
-              {/*    </div>*/}
-              {/*</div>*/}
-
               <Tabs>
                   <TabList>
                       <Tab>{this.sender}</Tab>
-                      {/*<Tab>Title 2</Tab>*/}
                   </TabList>
 
                   <TabPanel>
-                      <div className="message-box">
-                          <p>{message.body}</p>
-                      </div>
+                      {
+                          message.map((item, key) => {    // function(item) {
+                            console.log(key);
+                            // return item;
+                            return <div key={key} className="message-box"><p> {item.body} </p></div>;
+                          })
+                      }
+
+                      {/*<div className="message-box">*/}
+                      {/*    <p>{message.body}</p>*/}
+                      {/*</div>*/}
                       <div className="reply-box">
                         <textarea className="replyInput" value={reply} onChange={this.handleMessageChange} onKeyDown={this.handleKeyDown}/>
                       </div>
@@ -147,17 +123,6 @@ class WidgetContainer extends React.Component {
                   {/*</TabPanel>*/}
               </Tabs>
 
-            {/*  <div className="message-box">*/}
-            {/*    <p>{message.body}</p>*/}
-            {/*  </div>*/}
-            {/*<div className="reply-box">*/}
-            {/*    <textarea*/}
-            {/*        className="replyInput"*/}
-            {/*        value={reply}*/}
-            {/*        onChange={this.handleMessageChange}*/}
-            {/*        onKeyDown={this.handleKeyDown}*/}
-            {/*    />*/}
-            {/*</div>*/}
           </div>
         );
     }
